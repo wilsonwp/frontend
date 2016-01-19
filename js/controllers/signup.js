21bf7433c4324091c0a9bae82bbdd4419d6ce52f
Juanshot <garcia.solutions@gmail.com>
2016-01-19 11:10:47 -0430

    scope

--- js/controllers/signup.js
+++ js/controllers/signup.js
@@ -14,7 +14,7 @@ app.controller('SignupFormController', ['$scope', '$http','$state','SignResource
             console.log($scope.user);
             var uploadUrl='http://localhost:8000/hinchas';
             multipartForm.post(uploadUrl,$scope.user);
-            toaster.pop();
+            $scope.pop();
             
         
     }
